2f75508e760d8cb9100d7a7af86c4146cc6f4361
Mr-Adams <66141358+Mr-Adams@users.noreply.github.com>
2022-09-15 11:08:56 +0000

    nim added

--- py/binsearch.py
+++ py/binsearch.py
@@ -1,3 +1,9 @@
+# FILENAME binsearch.py
+# First Last Seth Adams
+# CSCI 77800 Fall 2022
+# collaborators: 
+# consulted: previous work from summer
+
 import random
 import math
 
---
+++ py/cgol.py
@@ -0,0 +1,5 @@
+# FILENAME cgol.py
+# First Last Seth Adams
+# CSCI 77800 Fall 2022
+# collaborators: 
+# consulted: previous work from summerimport random

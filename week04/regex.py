e1d331aff9a4e21ec3463f1c84be836fca6721d2
Mr-Adams <66141358+Mr-Adams@users.noreply.github.com>
2022-09-28 21:24:28 -0400

    Update regex.py

--- week04/regex.py
+++ week04/regex.py
@@ -2,10 +2,26 @@ import re
 
 
 def find_name(line):
-    pattern = r"([A-Za-z][-,A-Za-z. ']+[ ]*)+"
+    #this gives first and last names that are formated properly.
+    pattern = r'[A-Z][a-z]\w* [A-Z][a-z]\w*' 
     result = re.findall(pattern,line)
+     #This will take any common title and will print the properly formated name until a space
+    pattern = r'((?:Dr\.|Mr\.|Ms\.|Miss|Mr|Ms) [A-Z][a-z]\w*[A-Z][a-z]\w*)'
+    result = result + re.findall(pattern,line)
+  #First initial, last name
+    pattern = r'([A-Z]\s[A-Z][a-z]\w*)'
+    result = result + re.findall(pattern,line)
     return result
-
+    pattern = r'((?:Dr\.|Mr\.|Ms\.|Miss|Mr|Ms) [A-Z][a-z]\w*)'
+    result = result + re.findall(pattern,line)
+  #First initial, last name
+    pattern = r'([A-Z]\s[A-Z][a-z]\w*)'
+    result = result + re.findall(pattern,line)
+    return result
+  #inital and . followed by last name or last name followed by , and first name
+    pattern = r'([A-Z],\s*[A-Z][a-z]\w*)'
+    result = result + re.findall(pattern,line)
+    return result
 
 f = open("names.txt")
 for line in f.readlines():
